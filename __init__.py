ac6a4f161f5441b085562bc56feb9904681d5fd5
Conrad Irwin <conrad.irwin@gmail.com>
2010-06-10 17:04:12 +0100

    Initial commit

---
+++ __init__.py
@@ -0,0 +1,316 @@
+import re
+import os
+import xmlreader
+
+"""
+    A selection of tools for dealing with the English Wiktionary to a standard
+    that is "good enough" for whatever I needed when I wrote the function.
+
+    This is very much an evolving module, and no guarantees are made about 
+    maintaining functionality (or evn that existing functionality does what you
+    want).
+
+    Basic idea is to let this do lossless "parsing" and lookups and let the tools
+    make any modifications when they've got the data.
+
+    To this end "most" of the parsing functions return the entire input just split
+    into useful chunks.
+
+"""
+
+namespaces = set(['Media','Special','Talk','User','User talk','Wiktionary', 'Wiktionary talk', 'File', 'File talk', 'MediaWiki', 'MediaWiki talk', 'Template', 'Template talk', 'Help', 'Help talk', 'Category', 'Category talk', 'Appendix', 'Appendix talk', 'Concordance', 'Concordance talk', 'Index', 'Index talk', 'Rhymes', 'Rhymes talk', 'Transwiki', 'Transwiki talk', 'Wikisaurus', 'Wikisaurus talk', 'WT', 'WT talk', 'Citations', 'Citations talk'])
+
+posses = set(['noun', 'noun phrase', 'noun form', 'verb', 'verb form', 'verb phrase', 'transitive verb', 'intransitive verb', 'adjective', 'adjective form', 'adjective phrase', 'adverb', 'adverb phrase', 'pronoun', 'conjunction', 'contraction', 'interjection', 'preposition', 'proper noun', 'article', 'prefix', 'verb prefix', 'suffix', 'infix', 'interfix', 'circumfix', 'affix', 'idiom', 'phrase', 'acronym', 'abbreviation', 'initialism', 'symbol', 'letter', 'number', 'numeral', 'ordinal number', 'ordinal numeral', 'cardinal number', 'cardinal numeral', 'particle', 'proverb', 'han character', 'kanji', 'hanzi', 'hanja', 'pinyin', 'pinyin syllable', 'syllable', 'katakana character', 'hiragana letter', 'hiragana character', 'counter', 'classifier', 'adnominal', 'determiner', 'expression', 'postposition', 'root', 'participle', '{{initialism}}', '{{acronym}}', '{{abbreviation}}', 'cmavo', 'gismu'])
+
+re_language = re.compile(r"^(==\s*(?:\[\[\s*)?([^[=\s].*[^=\s\]])(?:\s*\]\])?\s*==)$", re.M)
+
+class Redirect(object):
+    """
+        Represents a (possible) redirect page.
+
+        .target = Null or the title of the target in the redirect
+        .redirect = "" or the fragment of wikitext that makes up the redirect (including one trailing newline)
+        .tail = "" or the fragment of wikitext that isn't part of the redirect
+
+        On well formed pages, .tail or .redirect should be ""
+
+        .redirect + .tail == the initial text
+    """
+
+    # !^\s*:?\s*\[{2}(.*?)(?:\|.*?)?\]{2}! (from includes/Title.php:newFromRedirectInternal)
+    re_redirect = re.compile(r"#REDIRECT\s*:?\s*\[{2}(.*?)(?:\|.*?)?\]{2}\n?")
+
+    def __init__(self, text):
+        m = self.re_redirect.match(text)
+
+        if m:
+            self.target = m.group(1)
+            self.redirect = m.group(0)
+            self.tail = text.replace(self.redirect, "", 1)
+        else:
+            self.target = None
+            self.redirect = ""
+            self.tail = text
+
+def split_redirect(text):
+    """
+    returns a Redirect object for the given text
+    """
+    return Redirect(text)
+
+
+def is_part_of_speech(title):
+    """
+    Is it on our part-of-speech whitelist
+    """
+    if title:
+        if title.lower() in posses or title.startswith('{{abbreviation') or title.startswith('{{initialism') or title.startswith('{{acronym'):
+            return True
+
+class Section(object):
+    """
+    Represents one language's section of a page.
+
+    The text will include the heading
+    """
+    def __init__(self, heading, text):
+        self.heading = heading
+        self.text = text
+
+    def __repr__(self):
+        return '"""' + self.text + '"""'
+
+def split_entry_trailer(text):
+    """
+    Split an entry into two parts, the bit with useful stuff, and the bit with categories, interwikis and ----
+
+    rejoining the two gives the original entry
+    """
+    re_useless = re.compile(r"^ *(----|\[\[ *[Ca-z\-]* *:[^\]]*\]\]|\{\{count page[^\}]*\}\})? *$")
+    useful = []
+    useless = []
+    for line in text.split("\n"):
+        if re_useless.match(line):
+            useless.append(line)
+        else:
+            useful += useless
+            useful.append(line)
+            useless = []
+
+    return ("\n".join(useful) + "\n", "\n".join(useless))
+
+class NotUniqueException(Exception):
+    def __init__(self, lst):
+        self.message = "More than one 'unique' section"
+        self.lst = lst
+
+def unique_section(heading, text):
+    """
+    Assume there is only one section with a given title on the page, and return it
+    """
+    base_re = r"^(=+\s*(?:\[\[\s*)?(?:%s)(?:\s*\]\])?\s*=+)$" 
+    re_this_heading = re.compile(base_re % heading, re.M)
+    re_any_heading = re.compile(base_re % "[^[=\s].*[^=\s\]]", re.M)
+    
+    split = re_this_heading.split(text)
+    if len(split) == 1:
+        return Section(None, "")
+    elif len(split) >3:
+        raise NotUniqueException("More than one 'unique' section")
+    else:
+        splot = re_any_heading.split(split[2])
+        if len(splot) == 1:
+            return Section(heading, split[1] + split[2])
+        else:
+            return Section(heading, split[1] + splot[0])
+
+def all_sections(heading, text):
+    raise Exception("Please fix and document use")
+    try:
+        yield unique_section(heading, text)
+    except NotUniqueException, e:
+        base_re = r"^(=+\s*(?:\[\[\s*)?(?:%s)(?:\s*\]\])?\s*=+)$" 
+        re_any_heading = re.compile(base_re % "[^[=\s].*[^=\s\]]", re.M)
+        split = e.lst
+        for x in range(0,len(split),3):
+            splot = re_any_heading.split(split[x+2])
+            if len(splot) == 1:
+                yield Section(heading, split[x+1] + split[x+2])
+            else:
+                yield Section(heading, split[x+1] + splot[x+0])
+
+def all_subsections(text):
+    """
+        Get all the subsections of a language section (ignoring nesting)
+    """
+    re_any_heading = re.compile(r"^(=+\s*(?:\[\[\s*)?([^\[=\s].*[^=\s\]])(?:\s*\]\])?\s*=+)$", re.M)
+    split = re_any_heading.split(text)
+
+    if len(split) == 1:
+        yield Section(None, split[0])
+    else:
+        first = split.pop(0)
+        split[0] = first + split[0]
+        for x in range(1,len(split),3):
+            yield Section(split[x], split[x-1] + split[x+1])
+
+def all_headings(text, preserve_links=False):
+    if preserve_links:
+        base_re = r"^(=+\s*((?:\[\[\s*)?%s(?:\s*\]\])?)\s*=+)$" 
+    else:
+        base_re = r"^(=+\s*(?:\[\[\s*)?(%s)(?:\s*\]\])?\s*=+)$" 
+    re_any_heading = re.compile(base_re % "[^[=\s].*[^=\s\]]", re.M)
+    
+    for m in re_any_heading.findall(text):
+        yield m[1]
+
+def language_sections(text):
+    """
+    Generate LanguageSections from an entry (really splits on == headings)
+
+    If you join together the "text" of these you are guaranteed to re-assemble
+    the original entry, anything before the first heading is lumped into the first heading.
+
+    If the entry is unparsable, the language will be `None`
+    """
+
+    split = re_language.split(text)
+    if len(split) == 1:
+        yield Section(None, split[0])
+
+    else:
+        split[1] = split[0] + split[1]
+        x = 1
+        while x < len(split):
+            yield Section(split[x+1], split[x] + split[x+2])
+            x += 3
+
+def filter_language(language, entry, editor):
+    raise Exception("Broken")
+
+    output = ""
+    for section in language_sections(entry.text):
+        if section.heading == language:
+            output += editor(entry, section) or section.text
+        else:
+            output += section.text
+
+    return output
+
+def filter_heading(heading, entry, editor):
+    raise Exception("Broken")
+
+    output = ""
+    for section in all_headings(entry):
+        if section.heading == heading or section.heading.startswith(heading + " "):
+            output += editor(section.text) or section.text
+        else:
+            otuput += section.text
+
+    return output
+
+def definition_lines(text):
+    """
+    Get all lines that look like definition lines
+    """
+    return [line for line in text.split("\n") if len(line) > 2 and line[0] == "#" and line[1] not in ":*#*"]
+
+def latest_dump(offline=False, helper="../dumps/latest_dump.sh", __cache=[]):
+    """
+        Returns the path to the latest dump file according to `helper`
+        This function is guaranteed to return the same path throughout the 
+        lifetime of program execution.
+
+        WARNING: This function may cause two HTTP requests, one to get the
+        latest available version, and the other to download the dump itself.
+    """
+    if len(__cache):
+        return __cache[0];
+
+    if offline:
+        stdin = os.popen(helper + " --offline")
+        return stdin.read().strip()
+    
+    else:
+        stdin = os.popen(helper)
+        __cache.append(stdin.read().strip())
+        return latest_dump()
+
+def dump_entries(dump=None, namespace=None, main_only=False, offline=False):
+    """
+        Returns an iterator over every entry in the `dump`
+        If the `dump` is not specified, `latest_dump()` is used.
+    """
+    if dump is None:
+        dump = latest_dump(offline=offline)
+
+    def do_main_only(namespaces):
+        for entry in xmlreader.XmlDump(dump).parse():
+            if not ":" in entry.title:
+                yield entry
+            elif not entry.title[:entry.title.find(":")] in namespaces:
+                yield entry
+    
+    def do_namespace(namespace):
+        for entry in xmlreader.XmlDump(dump).parse():
+            if entry.title[:entry.title.find(":")] == namespace:
+                yield entry
+
+    if main_only == False and namespace != "":
+        if namespace:
+            return do_namespace(namespace)
+        else:
+            return xmlreader.XmlDump(dump).parse()
+    else:
+        return do_main_only(namespaces)
+
+def talk_page(title):
+    """
+        Gets the talk page for the given title.
+        (on en.wiktionary)
+    """
+
+    title = title.replace("_", " ")
+    if title.startswith(":"):
+        title = title[1:]
+
+    parts = title.split(":", 1)
+
+    if len(parts) == 1:
+        return "Talk:" + parts[0]
+    else:
+        if parts[0] in namespaces:
+            if parts[0] in ['Talk', 'Citations talk', 'Citations']:
+                return "Talk:" + parts[1]
+            if parts[0] in ['Template', 'Template talk'] and parts[1].endswith("/doc"):
+                parts[1] = re.sub("/doc$", "", parts[1])
+            if parts[0].lower().endswith("talk"):
+                return title
+            else:
+                return parts[0] + " talk" + ":" + parts[1]
+
+        else:
+            return "Talk:" + parts[0] + ":" + parts[1]
+
+re_templated = re.compile(r"(^#* *\{\{.*\}\} *$|\{\{form of|\{\{infl)")
+re_not_templated = re.compile(r"([pP]resent|[Pp]erfect|[Pp]lural|[Ss]ingular|[Pp]ast historic|ive|[Pp]reterite|[Cc]ompound)(\]\])?[^a-zA-Z]*form of[ ']*\[\[")
+re_compound = re.compile(r" *[Cc]ompound of[^a-zA-Z]*\[\[")
+
+def is_form_of(line):
+    """
+        Try to guess whether a defn is a form-of.
+
+        Hacky regexes from obsolete awk scripts
+
+        FIXME: this should be made good....
+    """
+    if re_templated.search(line):
+        return ("given name" not in line and "SI-unit" not in line)
+
+    if re_not_templated.search(line):
+        return True
+
+    if re_compound.search(line):
+        return True
+
+    return False
---
+++ xmlreader.py
@@ -0,0 +1,359 @@
+# -*- coding: utf-8  -*-
+
+"""
+Each XmlEntry object represents a page, as read from an XML source
+
+The MediaWikiXmlHandler can be used for the XML given by Special:Export
+as well as for XML dumps.
+
+The XmlDump class reads a pages_current XML dump (like the ones offered on
+http://download.wikimedia.org/wikipedia/de/) and offers a generator over
+XmlEntry objects which can be used by other bots.
+
+For fastest processing, XmlDump uses the cElementTree library if available
+(this comes included with Python 2.5, and can be downloaded from
+http://www.effbot.org/ for earlier versions). If not found, it falls back
+to the older method using regular expressions.
+"""
+__version__='$Id: xmlreader.py 5775 2008-08-01 13:51:21Z wikipedian $'
+
+import threading, time
+import xml.sax
+import codecs, re
+
+try:
+    from xml.etree.cElementTree import iterparse
+except ImportError:
+    try:
+        from cElementTree import iterparse
+    except ImportError:
+        pass
+
+def parseRestrictions(restrictions):
+    """
+    Parses the characters within a restrictions tag and returns
+    strings representing user groups allowed to edit and to move
+    a page, where None means there are no restrictions.
+    """
+    if not restrictions:
+        return None, None
+    editRestriction = None
+    moveRestriction = None
+    editLockMatch = re.search('edit=([^:]*)', restrictions)
+    if editLockMatch:
+        editRestriction = editLockMatch.group(1)
+    moveLockMatch = re.search('move=([^:]*)', restrictions)
+    if moveLockMatch:
+        moveRestriction = moveLockMatch.group(1)
+    if restrictions == 'sysop':
+        editRestriction = 'sysop'
+        moveRestriction = 'sysop'
+    return editRestriction, moveRestriction
+
+
+class XmlEntry:
+    """
+    Represents a page.
+    """
+    def __init__(self, title, id, text, username, ipedit, timestamp, editRestriction, moveRestriction, revisionid):
+        # TODO: there are more tags we can read.
+        self.title = unicode(title)
+        self.name = unicode(title)
+        self.id = int(id)
+        self.text = unicode(text)
+        self.username = username.strip()
+        self.ipedit = ipedit
+        self.timestamp = timestamp
+        self.editRestriction = editRestriction
+        self.moveRestriction = moveRestriction
+        self.revisionid = revisionid
+
+
+class XmlHeaderEntry:
+    """
+    Represents a header entry
+    """
+    def __init__(self):
+        self.sitename = u''
+        self.base = u''
+        self.generator = u''
+        self.case = u''
+        self.namespaces = {}
+
+
+class MediaWikiXmlHandler(xml.sax.handler.ContentHandler):
+    def __init__(self):
+        xml.sax.handler.ContentHandler.__init__(self)
+        self.inRevisionTag = False
+        self.inContributorTag = False
+        self.headercallback = None
+        # Older Mediawiki version sometimes do not have these elements.
+        # They are initialized here so they at least have some value when
+        # asked for
+        self.id = u''
+        self.revisionid = u''
+
+    def setCallback(self, callback):
+        self.callback = callback
+
+    def setHeaderCallback(self, headercallback):
+        self.headercallback = headercallback
+
+    def startElement(self, name, attrs):
+        self.destination = None
+        if name == 'page':
+            self.editRestriction = None
+            self.moveRestriction = None
+            self.ipedit = None
+        elif name == 'revision':
+            self.inRevisionTag = True
+        elif name == 'contributor':
+            self.inContributorTag = True
+        elif name == 'text':
+            self.destination = 'text'
+            self.text=u''
+        elif name == 'id':
+            if self.inContributorTag:
+                self.destination = 'userid'
+                self.userid = u''
+            elif self.inRevisionTag:
+                self.destination = 'revisionid'
+                self.revisionid = u''
+            else:
+                self.destination = 'id'
+                self.id = u''
+        elif name == 'username':
+            self.destination = 'username'
+            self.username = u''
+            self.ipedit = False
+        elif name == 'ip':
+            self.destination = 'username'  # store it in the username
+            self.username = u''
+            self.ipedit = True
+        elif name == 'restrictions':
+            self.destination = 'restrictions'
+            self.restrictions = u''
+        elif name == 'title':
+            self.destination = 'title'
+            self.title=u''
+        elif name == 'timestamp':
+            self.destination = 'timestamp'
+            self.timestamp=u''
+        elif self.headercallback:
+            if name == 'siteinfo':
+                self.header = XmlHeaderEntry()
+            elif name in ['sitename', 'base', 'generator', 'case']:
+                self.destination = name
+            elif name == 'namespace':
+                self.destination = 'namespace'
+                self.namespace = u''
+                self.namespaceid = int(attrs['key'])
+
+    def endElement(self, name):
+        if name == 'contributor':
+            self.inContributorTag = False
+        elif name == 'restrictions':
+            self.editRestriction, self.moveRestriction = parseRestrictions(self.restrictions)
+        elif name == 'revision':
+            # All done for this.
+            text = self.text
+            # Remove trailing newlines and spaces
+            while text and text[-1] in '\n ':
+                text = text[:-1]
+            # Replace newline by cr/nl
+            text = u'\r\n'.join(text.split('\n'))
+            # Decode the timestamp
+            timestamp = (self.timestamp[0:4]+
+                         self.timestamp[5:7]+
+                         self.timestamp[8:10]+
+                         self.timestamp[11:13]+
+                         self.timestamp[14:16]+
+                         self.timestamp[17:19])
+            self.title = self.title.strip()
+            # Report back to the caller
+                
+            entry = XmlEntry(self.title, self.id, text, self.username, self.ipedit, timestamp, self.editRestriction, self.moveRestriction, self.revisionid)
+            self.inRevisionTag = False
+            self.callback(entry)
+        elif self.headercallback:
+            if name == 'namespace':
+                self.header.namespaces[self.namespace] = self.namespaceid
+            elif name == 'siteinfo':
+                self.headercallback(self.header)
+                self.header = None
+        # Characters between this closing tag and the next opening tag is
+        # ignored, it's just whitespace for XML formatting.
+        self.destination = None
+
+    def characters(self, data):
+        if self.destination == 'text':
+            self.text += data
+        elif self.destination == 'id':
+            self.id += data
+        elif self.destination == 'revisionid':
+            self.revisionid += data
+        elif self.destination == 'restrictions':
+            self.restrictions += data
+        elif self.destination == 'title':
+            self.title += data
+        elif self.destination == 'username':
+            self.username += data
+        elif self.destination == 'timestamp':
+            self.timestamp += data
+        elif self.headercallback:
+            if self.destination == 'sitename':
+                self.header.sitename += data
+            elif self.destination == 'base':
+                self.header.base += data
+            elif self.destination == 'generator':
+                self.header.generator += data
+            elif self.destination == 'case':
+                self.header.case += data
+            elif self.destination == 'namespace':
+                self.namespace += data
+
+
+class XmlParserThread(threading.Thread):
+    """
+    This XML parser will run as a single thread. This allows the XmlDump
+    generator to yield pages before the parser has finished reading the
+    entire dump.
+
+    There surely are more elegant ways to do this.
+    """
+    def __init__(self, filename, handler):
+        threading.Thread.__init__(self)
+        self.filename = filename
+        self.handler = handler
+
+    def run(self):
+        xml.sax.parse(self.filename, self.handler)
+
+
+class XmlDump(object):
+    """
+    Represents an XML dump file. Reads the local file at initialization,
+    parses it, and offers access to the resulting XmlEntries via a generator.
+
+    NOTE: This used to be done by a SAX parser, but the solution with regular
+    expressions is about 10 to 20 times faster. The cElementTree version is
+    again much, much faster than the regex solution.
+    """
+    def __init__(self, filename):
+        self.filename = filename
+
+    def parse(self):
+        """Return a generator that will yield XmlEntry objects"""
+        if not 'iterparse' in globals():
+            return self.regex_parse()
+        else:
+            return self.new_parse()
+
+    def new_parse(self):
+        """Generator using cElementTree iterparse function"""
+        if self.filename.endswith('.bz2'):
+            import bz2
+            source = bz2.BZ2File(self.filename)
+        else:
+            # assume it's an uncompressed XML file
+            source = open(self.filename)
+        context = iterparse(source, events=("start", "end", "start-ns"))
+        root = None
+
+        for event, elem in context:
+            if event == "start-ns" and elem[0] == "":
+                uri = elem[1]
+                continue
+            if event == "start" and root is None:
+                root = elem
+                continue
+            if event == "end" and elem.tag == "{%s}page" % uri:
+                title = elem.findtext("{%s}title" % uri)
+                pageid = elem.findtext("{%s}id" % uri)
+                restrictions = elem.findtext("{%s}restrictions" % uri)
+                revision = elem.find("{%s}revision" % uri)
+                revisionid = revision.findtext("{%s}id" % uri)
+                timestamp = revision.findtext("{%s}timestamp" % uri)
+                contributor = revision.find("{%s}contributor" % uri)
+                ipeditor = contributor.findtext("{%s}ip" % uri)
+                username = ipeditor or contributor.findtext("{%s}username" % uri)
+                # could get comment, minor as well
+                text = revision.findtext("{%s}text" % uri)
+                editRestriction, moveRestriction \
+                        = parseRestrictions(restrictions)
+                yield XmlEntry(title=title, id=pageid, text=text or u'',
+                               username=username, ipedit=bool(ipeditor),
+                               timestamp=timestamp,
+                               editRestriction=editRestriction,
+                               moveRestriction=moveRestriction,
+                               revisionid=revisionid
+                              )
+                root.clear()
+
+    def regex_parse(self):
+        """
+        Generator which reads some lines from the XML dump file, and
+        parses them to create XmlEntry objects. Stops when the end of file is
+        reached.
+
+        NOTE: This is very slow. It's only a fallback solution for users who
+        haven't installed cElementTree.
+        """
+        Rpage = re.compile(
+            '<page>\s*'+
+            '<title>(?P<title>.+?)</title>\s*'+
+            '<id>(?P<pageid>\d+?)</id>\s*'+
+            '(<restrictions>(?P<restrictions>.+?)</restrictions>)?\s*'+
+            '<revision>\s*'+
+              '<id>(?P<revisionid>\d+?)</id>\s*'+
+              '<timestamp>(?P<timestamp>.+?)</timestamp>\s*'+
+              '<contributor>\s*'+
+                '(<username>(?P<username>.+?)</username>\s*'+
+                '<id>(?P<userid>\d+?)</id>|<ip>(?P<ip>.+?)</ip>)\s*'+
+              '</contributor>\s*'+
+              '(?P<minor>(<minor />))?\s*'+
+              '(?:<comment>(?P<comment>.+?)</comment>\s*)?'+
+              '(<text xml:space="preserve">(?P<text>.*?)</text>|<text xml:space="preserve" />)\s*'+
+            '</revision>\s*'+
+            '</page>',
+                re.DOTALL)
+        f = codecs.open(self.filename, 'r', encoding = 'utf-8',
+                        errors='replace')
+        eof = False
+        lines = u''
+        while not eof:
+            line = f.readline()
+            lines += line
+            if line == '':
+                eof = True
+            elif line.endswith(u'</page>\n'):
+                # unescape characters
+                lines = lines.replace('&gt;', '>')
+                lines = lines.replace('&lt;', '<')
+                lines = lines.replace('&quot;', '"')
+                lines = lines.replace('&amp;', '&')
+                m = Rpage.search(lines)
+                if not m:
+                    print >>sys.stderr, 'ERROR: could not parse these lines:'
+                    print >>sys.stderr, lines
+                    lines = u''
+                else:
+                    lines = u''
+                    text = m.group('text') or u''
+                    restrictions = m.group('restrictions')
+                    editRestriction, moveRestriction = parseRestrictions(restrictions)
+
+                    if m.group('username'):
+                        username = m.group('username')
+                        ipedit = False
+                    else:
+                        username = m.group('ip')
+                        ipedit = True
+                    yield XmlEntry(title = m.group('title'),
+                                   id=m.group('pageid'), text=text,
+                                   username=username, ipedit=ipedit,
+                                   timestamp=m.group('timestamp'),
+                                   editRestriction = editRestriction,
+                                   moveRestriction=moveRestriction,
+                                   revisionid=m.group('revisionid')
+                                  )
